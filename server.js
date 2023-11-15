59a45b0d769bcda6c91d50552ab963342bc60693
LandonP172 <141693593+LandonP172@users.noreply.github.com>
2023-11-14 20:57:25 -0700

    fixed notes

--- server.js
+++ server.js
@@ -11,21 +11,20 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-// GET Route for homepage
-app.get('/', (req, res) =>
+app.get('/notes', (req, res) =>
 res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET Route for feedback page
-app.get('/feedback', (req, res) =>
-res.sendFile(path.join(__dirname, '/public/pages/feedback.html'))
+app.get('/api/notes', (req, res) =>
+res.sendFile(path.join(__dirname, '/db/db.json'))
 );
 
 // Wildcard route to direct users to a 404 page
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+res.sendFile(path.join(__dirname, 'public/index.html'))
 );
 
 app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT}`)
+console.log(`App listening at http://localhost:${PORT}`)
 );
